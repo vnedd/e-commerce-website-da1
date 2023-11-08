db77268836f4b7d7e9c1667d8a658de22eac3e72
vnedd <thangvu2098@gmail.com>
2023-11-08 08:48:31 +0700

    test

--- admin/index.php
+++ admin/index.php
@@ -12,25 +12,21 @@
 </head>
 
 <body>
-    <div class="container py-2">
-
+    <div class="">
+        <?php include('./sidebar.php') ?>
         <?php
-        include 'header.php';
-        ?>
-        <div class="content mt-4">
-            <?php
-            if (isset($_GET['act'])) {
-                $act = $_GET['act'];
-                switch ($act) {
-                    default:
-                        include 'home.php';
-                }
-            } else {
-                include 'home.php';
+        if (isset($_GET['act'])) {
+            $act = $_GET['act'];
+            switch ($act) {
+                default:
+                    include 'home.php';
             }
+        } else {
+            include 'home.php';
+        }
 
-            ?>
-        </div>
+        ?>
+    </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
--- model/pdo.php
+++ model/pdo.php
@@ -2,7 +2,7 @@
 
 function pdo_get_connection()
 {
-    $dburl = "mysql:host=localhost;dbname=duanmau;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=duan1;charset=utf8";
     $username = 'root';
     $password = '';
     $conn = new PDO($dburl, $username, $password);
