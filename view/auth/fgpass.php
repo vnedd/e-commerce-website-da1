eb37e2edd854e7eee3605ad08aed2ed87989ab05
khang ngo ~ <tonykhang995@gmail.com>
2023-12-06 11:54:22 +0700

    xong fix cứng

--- view/auth/fgpass.php
+++ view/auth/fgpass.php
@@ -10,7 +10,8 @@
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                         Email
                     </label>
-                    <input class="form-input text-sm rounded-md w-full" id="email" name="email" type="text" placeholder="Email">
+                    <input class="form-input text-sm rounded-md w-full" id="email" name="email" type="email" placeholder="Email...">
+                    <span class="form-message text-xs text-red-600 relative leading-3"></span>
                     <?php echo !empty($error['email']) ? '<span class="text-red-500 text-xs">' . $error['email'] . '</span>' : ""  ?>
 
                     <button class="btn md:btn-md btn-sm capitalize bg-slate-700 hover:bg-slate-800 w-full text-white mt-5" type="submit" name="sendMail">
---
+++ view/referencepage/refesite.php
@@ -0,0 +1,45 @@
+<!-- About 1 - Bootstrap Brain Component -->
+<section class="py-3 py-md-5 py-xl-8">
+  <div class="container">
+    <div class="row gy-3 gy-md-4 gy-lg-0 align-items-lg-center">
+      <div class="col-12 col-lg-6 col-xl-5">
+        <img class="img-fluid rounded" loading="lazy" src="./upload/about-img-1.jpg" alt="">
+      </div>
+      <div class="col-12 col-lg-6 col-xl-7">
+        <div class="row justify-content-xl-center">
+          <div class="col-12 col-xl-11">
+            <h2 class="h1 mb-3">Who Are We?</h2>
+            <p class="lead fs-4 text-secondary mb-3">We provide remarkable and latest products and custom to find suitable items. Our perspective is to furnish outstanding captivating services.</p>
+            <p class="mb-5">At TKQ SHOP, every product we make is built to last. We design durable, easy-to-use devices with innovative features that customers depend on, all while protecting their privacy and data. Customers should have access to safe, reliable, and secure repairs with genuine Apple parts if they need them..</p>
+            <div class="row gy-4 gy-md-0 gx-xxl-5X">
+              <div class="col-12 col-md-6">
+                <div class="d-flex">
+                 
+                    <img src="./assets/img/logo.svg" style="width: 50px ; height: 50px ;" alt="" srcset="">
+                
+                  <div>
+                    <h4 class="mb-3">Versatile Brand</h4>
+                    <p class="text-secondary mb-0">We are crafting a digital method that subsists life across all mediums.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="d-flex">
+                  <div class="me-4 text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-fire" viewBox="0 0 16 16">
+                      <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h4 class="mb-3">Digital Agency</h4>
+                    <p class="text-secondary mb-0">We believe in innovation by merging primary with elaborate ideas.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
